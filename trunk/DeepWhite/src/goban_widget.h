1913bdace5bec8c33931c9a5184f7f15c7650ccf
matyaspcsaba <matyaspcsaba@6351efc7-284f-0410-805e-19b79f695da6>
2009-06-24 20:21:16 +0000

    DeepWhite project

---
+++ trunk/DeepWhite/src/goban_widget.h
@@ -0,0 +1,15 @@
+#ifndef GOBAN_WIDGET_H
+#define GOBAN_WIDGET_H
+
+#include <QPixmap>
+#include <QWidget>
+
+class Goban : public QWidget {
+    Q_OBJECT
+public:
+    Goban(QWidget *);
+private:
+}
+
+
+#endif // GOBAN_WIDGET_H
