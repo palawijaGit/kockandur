1aecb1a868f88f1c31aa6ddab1edd78d0bffdbca
matyaspcsaba <matyaspcsaba@6351efc7-284f-0410-805e-19b79f695da6>
2010-10-20 09:53:48 +0000

    Proba valami

--- Proba/Proba.cpp
+++ Proba/Proba.cpp
@@ -23,7 +23,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, const char** argv) 
 {
-    std::cout << "This is a test" << std::endl;
+    unsigned char * image;
+    unsigned int  width;
+    unsigned int  height; 
+    unsigned int  channels;
+    loadPPM(argv[1], &image, &width, &height, &channels) 
+    
+        std::cout << "This is a test" << std::endl;
 
     cl_int errorNum = CL_SUCCESS; 
     cl_uint deviceCount = 0;
@@ -86,6 +92,107 @@ int main(int argc, const char** argv)
     free(source);
 
     return 0;
+}
+
+const unsigned int PGMHeaderSize = 0x40;
+
+bool loadPPM(const char* file, unsigned char** data, 
+            unsigned int *w, unsigned int *h, unsigned int *channels) 
+{
+    FILE* fp = 0;
+
+    if ((fp = fopen(file, "rb")) == 0)
+        {
+            // if error on attempt to open, be sure the file is null or close it, then return negative error code
+            if (fp)
+                {
+                    fclose (fp);
+                }
+            std::cerr << "loadPPM() : Failed to open file: " << file << std::endl;
+            return false;
+        }
+    
+    // check header
+    char header[PGMHeaderSize];
+    if ((fgets( header, PGMHeaderSize, fp) == NULL) && ferror(fp)) {
+        if (fp)
+            {
+                fclose (fp);
+            }
+        std::cerr << "loadPPM() : File is not a valid PPM or PGM image" << std::endl;
+        *channels = 0;
+        return false;
+    }
+    
+    if (strncmp(header, "P5", 2) == 0)
+    {
+        *channels = 1;
+    }
+    else if (strncmp(header, "P6", 2) == 0)
+    {
+        *channels = 3;
+    }
+    else
+    {
+        std::cerr << "loadPPM() : File is not a PPM or PGM image" << std::endl;
+        *channels = 0;
+        return shrFALSE;
+    }
+
+    // parse header, read maxval, width and height
+    unsigned int width = 0;
+    unsigned int height = 0;
+    unsigned int maxval = 0;
+    unsigned int i = 0;
+    while(i < 3) 
+    {
+        if ((fgets(header, PGMHeaderSize, fp) == NULL) && ferror(fp))
+        {
+            if (fp)
+            {
+                fclose (fp);
+            }
+            std::cerr << "loadPPM() : File is not a valid PPM or PGM image" << std::endl;
+            return shrFALSE;
+        }
+        if(header[0] == '#') continue;
+
+        if(i == 0) {
+            i += sscanf(header, "%u %u %u", &width, &height, &maxval);
+        } else if (i == 1) {
+            i += sscanf(header, "%u %u", &height, &maxval);
+        } else if (i == 2) {
+            i += sscanf(header, "%u", &maxval);
+        }
+    }
+
+    // check if given handle for the data is initialized
+    if(NULL != *data) 
+    {
+        if (*w != width || *h != height) 
+        {
+            fclose(fp);
+            std::cerr << "loadPPM() : Invalid image dimensions." << std::endl;
+            return false;
+        }
+    } 
+    else 
+    {
+        *data = (unsigned char*)malloc( sizeof(unsigned char) * width * height * *channels);
+        *w = width;
+        *h = height;
+    }
+
+    // read and close file
+    if (fread(*data, sizeof(unsigned char), width * height * *channels, fp) != width * height * *channels)
+    {
+        fclose(fp);
+        std::cerr << "loadPPM() : Invalid image." << std::endl;
+        return false;
+    }
+    fclose(fp);
+
+    return true;
 }
 
 void SobelOperator() { 
